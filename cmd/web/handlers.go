34895b5ac879b7c9a54b62e174a7127caabcb628
James <james.irish18@gmail.com>
2024-05-19 22:45:58 +0100

    add ui and makes home handler serve it

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,14 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strconv"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Server", "Go")
-    w.Write([]byte("Hello from Snippetbox"))
+
+    // Include the navigation partial in the template files.
+    files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/html/partials/nav.tmpl",
+        "./ui/html/pages/home.tmpl",
+    }
+
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
+    err = ts.ExecuteTemplate(w, "base", nil)
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    }
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
