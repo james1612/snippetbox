1366c75a5ead78be49ccbf67abe39f3cbf2fc1b9
James <james.irish18@gmail.com>
2024-05-24 14:19:16 +0100

    add sql driver and establish connection to mysql in main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,6 +17,15 @@ type application struct {
 func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.LUTC|log.Lshortfile)
+
+	dsn := "root:password@/snippetbox?parseTime=true"
+	db, err := openDB(dsn)
+
+    if err != nil {
+        errorLog.Fatal(err)
+	}
+
+    defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -28,6 +40,19 @@ func main() {
 		Handler:  app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+
+    if err != nil {
+        return nil, err
+    }
+
+    if err = db.Ping(); err != nil {
+        return nil, err
+}
+    return db, nil
 }
